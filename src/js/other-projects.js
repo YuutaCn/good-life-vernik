e750491f73ad5cdcc9458a86ae403a4f58778faa
yuutacn <yuutacn@gmail.com>
2023-03-13 16:26:58 +0300

    fix

--- src/js/other-projects.js
+++ src/js/other-projects.js
@@ -1,4 +1,4 @@
-document.querySelector('.projects .projects__title').addEventListener('click', () => {
-  document.querySelector('.projects .projects__list').classList.toggle('list_active');
-  document.querySelector('.projects .projects__title').classList.toggle('projects__title_active');
+document.querySelector('.projects__title').addEventListener('click', () => {
+  document.querySelector('.projects__list').classList.toggle('projects__list_active');
+  document.querySelector('.projects__title').classList.toggle('projects__title_active');
 });
